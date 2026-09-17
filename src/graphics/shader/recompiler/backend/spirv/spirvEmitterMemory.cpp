e2080d72c0214db9ed0f6cc1fe3a3c9231f291ba
Lepexa-cell <emelianovdenis109@gmail.com>
2026-09-17 21:03:24 +0000

    gpu: добавить обработку Snorm для 16-битных компонентов в функции StoreFormattedInBounds

--- src/graphics/shader/recompiler/backend/spirv/spirvEmitterMemory.cpp
+++ src/graphics/shader/recompiler/backend/spirv/spirvEmitterMemory.cpp
@@ -928,6 +928,11 @@ void StoreFormattedInBounds(ValueEmitContext& ctx, const IR::MemoryInfo& mem,
                             uint32_t data) {
 	if (component >= plan.info.component_count) return;
 	const auto bits = plan.info.component_bits[component];
+	if (plan.info.type == Format::ComponentType::Snorm && bits == 16u) {
+		const auto value = EmitBitCastF32U32(ctx.state, data);
+		data = EmitPackSnorm2x16(
+		    ctx.state, EmitCompositeConstructF32x2(ctx.state, value, ConstantF32Value(ctx.state, 0.0f)));
+	}
 	if (bits == 8u || bits == 16u) {
 		StoreSubwordInBounds(ctx, mem, plan.resource, plan.addresses[component],
 		                     plan.indices[component], bits, data);
